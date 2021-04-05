9c6213f27ade4040e41fee3a0428cc8b7a7bb7ba
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-04-05 21:02:18 +0500

    Ruby School Lesson 21

---
+++ app.rb
@@ -0,0 +1,49 @@
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  unless session[:identity] == 'admin' && session[:password] == '12345'
+    session[:previous_url] = request.path
+    @error = 'Sorry, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+end
+
+get '/' do
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+end
+
+get '/login/form' do
+  erb :login_form
+end
+
+post '/login/attempt' do
+  session[:identity] = params['username']
+  session[:password] = params['pass']
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from
+end
+
+get '/logout' do
+  session.delete(:identity)
+  session.delete(:password)
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+get '/secure/place' do
+	if session[:identity] == 'admin' && session[:password] == '12345'
+  		erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+	else
+		redirect to '/'
+	end	
+end
